2a459fa655d6aba82b289ebb0656181cdcbb89f7
Cristian Herrera <cristiandherrera95@gmail.com>
2021-08-26 23:57:48 -0700

    adding api DEMO key

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const API_KEY = "DEMO KEY";
+const API_URL = `pi.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+
 const weatherButton = document.querySelector(".show-previous-weather-button");
 const previousWeather = document.querySelector(".previous-weather");
 
