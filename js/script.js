ebf86239accfd6c6db90db972e781c0643cd8665
Cristian Herrera <cristiandherrera95@gmail.com>
2021-10-25 21:47:59 -0700

    removed modules

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = "DEMO KEY";
-const API_URL = `pi.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
-
-const weatherButton = document.querySelector(".show-previous-weather-button");
-const previousWeather = document.querySelector(".previous-weather");
-
-const animation = weatherButton.addEventListener("click", () => {
-  previousWeather.classList.toggle("show-weather");
-});
